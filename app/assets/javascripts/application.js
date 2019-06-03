2a4868f37132bcec51ebcaed1d92cd463101e177
Toww <th.guilmin@gmail.com>
2019-06-03 21:58:03 +0200

    WIP bootstrap installation, CSS JS CALL OK

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,50 +12,10 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
 
 // ---------- TEMPLATE'S JS -----------
-//= require js/bootstrap/alert.js
-//= require js/bootstrap/button.js
-//= require js/bootstrap/carousel.js
-//= require js/bootstrap/collapse.js
-//= require js/bootstrap/dropdown.js
-//= require js/bootstrap/index.js
-//= require js/bootstrap/modal.js
-//= require js/bootstrap/popover.js
-//= require js/bootstrap/scrollspy.js
-//= require js/bootstrap/tab.js
-//= require js/bootstrap/toast.js
-//= require js/bootstrap/tooltip.js
-//= require js/bootstrap/util.js
-//= require js/bootstrap/tools/sanitizer.js
-//= require js/mrare/aos.js
-//= require js/mrare/background-images.js
-//= require js/mrare/countdown.js
-//= require js/mrare/countup.js
-//= require js/mrare/dropdown-grid.js
-//= require js/mrare/fade-page.js
-//= require js/mrare/flatpickr.js
-//= require js/mrare/flickity.js
-//= require js/mrare/form-email.js
-//= require js/mrare/index.js
-//= require js/mrare/ion-rangeslider.js
-//= require js/mrare/isotope.js
-//= require js/mrare/jarallax.js
-//= require js/mrare/maps-style.js
-//= require js/mrare/overlay-nav.js
-//= require js/mrare/plyr.js
-//= require js/mrare/popovers.js
-//= require js/mrare/prism.js
-//= require js/mrare/reading-position.js
-//= require js/mrare/maps.js
-//= require js/mrare/recaptcha-v2.js
-//= require js/mrare/smooth-scroll.js
-//= require js/mrare/sticky.js
-//= require js/mrare/twitter-fetcher.js
-//= require js/mrare/typed-text.js
-//= require js/mrare/util.js
-//= require js/mrare/wizard.js
 
+//= require js/bootstrap/index.js
+//= require js/mrare/index.js
 //= require_tree .
 
