e5ad59a989c947da918d322c510d108ce8683a6d
fralps <francois.loupias@orange.fr>
2019-06-04 14:03:44 +0200

    Edit, delete, show user OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:show] do
+  resources :users, only: [:show, :edit, :update, :destroy] do
     resources :avatars, only: [:create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
