0b87c3e28c2e399eafb099c1944fb81feee31504
CaroFG <carolina.ferreira131@gmail.com>
2019-06-04 20:51:24 +0200

    comments added to message system related models and controllers

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,12 +1,15 @@
 class ConversationsController < ApplicationController
 	before_action :set_conversation, except: [:index]
+	# We make sure that the user is actually authorized to view the requested conversation
 	before_action :check_participating!, except: [:index]
 	
 	def index
 	  @conversations = Conversation.participating(current_user).order('updated_at DESC')
 	end
-
+	 
 	def show
+	# We instantiate the @personal_message variable,	
+	# as it will be used inside the view to render a form (to send a new message) 
   @personal_message = PersonalMessage.new
 	end
 
--- app/controllers/personal_messages_controller.rb
+++ app/controllers/personal_messages_controller.rb
@@ -1,10 +1,9 @@
 class PersonalMessagesController < ApplicationController
+  # We check if a conversation between the users already exists 
 	before_action :find_conversation!
 
 	def new
-		puts "*"*100
-		puts "hola"
-		puts "*"*100
+    # If the conversation does exist, redirect to the “show conversation” action.
   	redirect_to conversation_path(@conversation) and return if @conversation
   	@personal_message = current_user.personal_messages.build
 	end
@@ -27,12 +26,19 @@ end
   end
 
   def find_conversation!
+  # If the :recipient_id is set (that is, the “send a message” link was clicked),  
   if params[:recipient_id]
+    # we try to find the other user to address the message.
     @recipient = User.find_by(id: params[:recipient_id])
+    # If the user was not found, redirect to the root path. 
+    # ---------FLASH ERROR TO DISPLAY -------------
     redirect_to(root_path) and return unless @recipient
+    # If the user was found, check if the conversation between him and the current user already exist.
     @conversation = Conversation.between(current_user.id, @recipient.id)[0]
   else
+    # Lastly, if the :receiver_id id is not set, we try to find an existing conversation by :conversation_id.
     @conversation = Conversation.find_by(id: params[:conversation_id])
+    # If there is no existing conversation between the users, we redirect to root_path
     redirect_to(root_path) and return unless @conversation && @conversation.participates?(current_user)
   end
 end
