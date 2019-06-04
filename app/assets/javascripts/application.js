e742de40e025322082a7f59ec124ebae0ea1689d
Toww <th.guilmin@gmail.com>
2019-06-04 15:24:00 +0200

    Basic index template working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,21 +1,44 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require rails-ujs
-//= require activestorage
+	// This is a manifest file that'll be compiled into application.js, which will include all the files
+	// listed below.
+	//
+	// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+	// vendor/assets/javascripts directory can be referenced here using a relative path.
+	//
+	// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+	// compiled file. JavaScript code in this file should be added after the last require_* statement.
+	//
+	// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+	// about supported directives.
+	//
 
-// ---------- TEMPLATE'S JS -----------
+	// ---------- TEMPLATE'S JS -----------
 
-//= require js/bootstrap/index.js
-//= require js/mrare/index.js
-//= require_tree .
+	//= require jquery.min
+	//= require popper.min
+	//= require bootstrap
 
+	//= require aos
+	//= require clipboard
+	//= require jquery.fancybox.min
+	//= require flatpickr.min
+	//= require flickity.pkgd.min
+	//= require ion.rangeSlider.min
+	//= require isotope.pkgd.min
+	//= require jarallax.min
+	//= require jarallax-video.min
+	//= require jarallax-element.min
+	//= require jquery.countdown.min
+	//= require jquery.smartWizard.min
+	//= require plyr.polyfilled.min
+	//= require prism
+	//= require scrollMonitor
+	//= require smooth-scroll.polyfills.min
+	//= require twitterFetcher_min
+	//= require typed.min
+	//= require theme
+
+
+	// ---------- BASE JS ----------
+	//= require rails-ujs
+	//= require activestorage
+	//= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # We define route to the homepage
+  root to: 'home#index'
+
+  # Route declarations :
+  resources :home, only: [:index]
 end
