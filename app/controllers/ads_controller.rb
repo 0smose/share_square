926fd867585eedfe68235f27808ff33343718740
fralps <francois.loupias@orange.fr>
2019-06-05 12:32:12 +0200

    Admin dashboard OK

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,4 +1,5 @@
 class AdsController < ApplicationController
+	before_action :is_validated, only: [:index]
 
 	def index
 		@ad = Ad.all.reverse
@@ -11,6 +12,7 @@ class AdsController < ApplicationController
 	def new
 		@ad = Ad.new
 	end
+
 	def create
 		@ad = Ad.create!(user_id: current_user.id, frequency: params[:frequency], duration: params[:duration], type: Type.find_by(name: params[:type]), category: Category.find_by(name: params[:category]), title: params[:title], description: params[:description], other_propositions: params[:other_propositions], availability: params[:availability])
 
@@ -43,18 +45,14 @@ class AdsController < ApplicationController
 		end
 	end
 
-		def destroy
-			@ad = Ad.find(params[:id])
-			@ad.destroy
-			redirect_to ads_path
-		end
-	end
-
-	def is_validated
+	def destroy
 		@ad = Ad.find(params[:id])
-		if @ad_path.validated != true
-			redirect_to root_path
-			flash[:alert] = "cet annonce n'as pas été validé"
-		end
-
+		@ad.destroy
+		redirect_to ads_path
 	end
+	
+	# Method to check if the ad is validated or not
+	def is_validated
+		@ads = Ad.all
+	end
+end
