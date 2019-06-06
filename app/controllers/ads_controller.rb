5bf425c80ef0e92d1b19b506ffefe782dfaba898
fralps <francois.loupias@orange.fr>
2019-06-06 10:03:04 +0200

    Bug create ad fix

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -15,13 +15,14 @@ class AdsController < ApplicationController
 	end
 
 	def create
-		@ad = Ad.create!(user_id: current_user.id, frequency: params[:frequency], duration: params[:duration], type: Type.find_by(name: params[:type]), category: Category.find_by(name: params[:category]), title: params[:title], description: params[:description], other_propositions: params[:other_propositions], availability: params[:availability])
+		@ad = Ad.new(user_id: current_user.id, frequency: params[:frequency], duration: params[:duration], type: Type.find_by(name: params[:type]), category: Category.find_by(name: params[:category]), title: params[:title], description: params[:description], other_propositions: params[:other_propositions], availability: params[:availability])
 
 		if @ad.save
 			redirect_to root_path
 			flash[:success] = "Votre annonce a été crée"
 		else
-			render 'new'
+			flash[:alert] = "Il y a un eu un problème..."
+			render :new
 		end
 	end
 
