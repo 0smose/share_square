f959c967660b718a82c061ca6bc25c287222eba0
Toww <th.guilmin@gmail.com>
2019-06-06 12:50:39 +0200

    Flash loading solved

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # Devise
+  devise_for :users
 
   # We define root to the homepage
   root to: 'static_pages#home'
