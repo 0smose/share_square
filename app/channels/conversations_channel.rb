ea47b00badabfeee6b26b83c93003b5db55ea99e
CaroFG <carolina.ferreira131@gmail.com>
2019-06-12 15:29:03 +0200

    comments added and slimmed code in partial

--- app/channels/conversations_channel.rb
+++ app/channels/conversations_channel.rb
@@ -9,13 +9,12 @@ class ConversationsChannel < ApplicationCable::Channel
 
   # This method is launched by conversations.coffee
   def speak(hash_from_conversations)
-
-
+    #We stock the conversation id, the user id and the user's input
   	conversation_id = hash_from_conversations['conversation_id'].to_i
 		user_id = hash_from_conversations['user_id'].to_i
-
   	text_content = hash_from_conversations['message']
-
+    
+    # We use the informations above to create the message in the database
   	message = PersonalMessage.create(body: text_content, conversation_id: conversation_id, user_id: user_id)
  
   	# We create a variable html which stocks the whole personnal_message partial with the message
